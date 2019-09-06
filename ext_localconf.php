76edd8a74b67355748b78897e4b548f36492bc22
Andriy Oprysko <pyvasyk@gmail.com>
2019-09-06 14:33:20 +0300

    [TASK] add X-Class

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,5 +2,8 @@
 defined('TYPO3_MODE') || die('Access denied.');
 
 (function () {
-
+    // X-Class image view helper
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3Fluid\Fluid\Core\ViewHelper\TagBuilder::class] = [
+        'className' => \Pixelant\PxaLazyLoadingImages\TagBuilder::class
+    ];
 })();
