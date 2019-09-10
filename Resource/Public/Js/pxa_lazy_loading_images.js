6d4bd0164b6b4acb1ab9b982a508f37ac1f05afb
Myroslav <myroslavlutsiv@gmail.com>
2019-09-10 13:45:37 +0300

    [BUGFIX] js changes

--- Resource/Public/Js/pxa_lazy_loading_images.js
+++ Resource/Public/Js/pxa_lazy_loading_images.js
@@ -1,17 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
-	let lazyImages = [].slice.call(document.querySelectorAll('img[data-src]'));
+
+  let lazyImages = [].slice.call(document.querySelectorAll('img[data-src]'));
 	if ('IntersectionObserver' in window) {
 		let lazyImageObserver = new IntersectionObserver(function (entries) {
 			entries.forEach(function (entry) {
 				if (entry.isIntersecting) {
-					let lazyImage = entry.target;
-					lazyImage.src = lazyImage.dataset.src;
-					lazyImage.classList.add('_ready');
-					lazyImageObserver.unobserve(lazyImage)
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.parentNode.classList.add('_ready')
+          lazyImageObserver.unobserve(lazyImage)
 				}
 			})
 		});
 		lazyImages.forEach(function (lazyImage) {
+      var wrapper = document.createElement('span');
+      lazyImage.parentNode.insertBefore(wrapper, lazyImage);
+      wrapper.className = 'lazy-load'
+      wrapper.appendChild(lazyImage);
 			lazyImageObserver.observe(lazyImage)
 		})
 	} else {
